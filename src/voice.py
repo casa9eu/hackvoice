8a23f80188a9e0c0757f2cad70d2f39c1afe7cbd
Sasha <lonagi22@gmail.com>
2020-10-09 21:36:20 +0300

    Update voice.py

--- src/voice.py
+++ src/voice.py
@@ -1,62 +1,44 @@
-#pip: pywin32, pypiwin32, pyttx3, SpeechRecognition, pipwin, pypiwin, fuzzywuzzy
-#pipwin: PyAudio
-
+import nvg
 import pyttsx3 as px
 import speech_recognition as sr
-from multiprocessing import Pool, freeze_support
-#from google import speak_g
-from time import sleep
-
-def savewords(my_words):
-    try:
-        with open("dialog.txt", "r") as f:
-            text = f.read()
-    except:
-        pass
-    with open("dialog.txt","w") as f:
-        f.write(text+"\n"+str(my_words))
-
-def speak(my_words):
-    voice.say(my_words)
-    voice.runAndWait()
-    voice.stop()
-
-def read(a=0):
-    with m as source:
-        print("[] Speak...")
-        #speak("*")
-        audio = r.listen(source, timeout=2, phrase_time_limit=200)
-    return r.recognize_google(audio, language="ru-RU").lower()
+from datetime import datetime as dt
 
 r = sr.Recognizer()
 m = sr.Microphone(device_index=1)
 voice = px.init()
+queue = []
 
-with m as source:
-    r.adjust_for_ambient_noise(source)
+def speak(text):
+    voice.say(text)
+    voice.runAndWait()
+    voice.stop()
 
-while True:
-    try:
-        # with Pool(processes=1) as pool:
-        #     my_words = pool.map(read,[], 10)
+def GetVoice(a=0):
+    with m as source:
+        print(f"[{dt.now().date()}, {str(dt.now().time())[:-7]}]:",end=" ")
+        audio = r.listen(source, timeout=20, phrase_time_limit=30)
+    queue.append(r.recognize_google(audio, language="ru-RU").lower())
 
-        my_words = read()
-        savewords(my_words)
-        print("[] *...")
-        print(":", my_words)
+if __name__ == "__main__":
+    with m as source:
+        r.adjust_for_ambient_noise(source)
 
-        # with Pool(processes=1) as pool:
-        #     pool.map(speak, my_words, 10)
+    voices = voice.getProperty('voices')
+    voice.setProperty('voice', voices[-1].id)
 
-        speak(my_words)
+    while True:
+        try:
+            GetVoice()
 
-        print("[] *...")
-    except sr.UnknownValueError:
-        print("Unknown Error")
-    except sr.WaitTimeoutError:
-        print("WaitTimeout Error")
-    except UnboundLocalError:
-        print("Not saved")
-    #sleep(1)
+            if (queue):
+                my_words = queue[-1]
+                nvg.FileSaver.Save(my_words)
+                print(my_words)
+                speak(my_words)
 
-freeze_support()
+        except sr.UnknownValueError:
+            print("Unknown Error")
+        except sr.WaitTimeoutError:
+            print("WaitTimeout Error")
+        except UnboundLocalError:
+            print("Not saved")
